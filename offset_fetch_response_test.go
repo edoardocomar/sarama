4ed857877df8504c8b18569c5a5f367c69c886a3
Edoardo Comar <ecomar@uk.ibm.com>
2018-10-11 11:56:32 +0100

    responses up to v5

--- offset_fetch_response_test.go
+++ offset_fetch_response_test.go
@@ -5,7 +5,12 @@ import "testing"
 var (
 	emptyOffsetFetchResponse = []byte{
 		0x00, 0x00, 0x00, 0x00}
+
 	emptyOffsetFetchResponseV2 = []byte{
+		0x00, 0x00, 0x00, 0x00,
+		0x00, 0x2A}
+
+	emptyOffsetFetchResponseV3 = []byte{
 		0x00, 0x00, 0x00, 0x00,
 		0x00, 0x2A}
 )
@@ -20,14 +25,14 @@ func TestEmptyOffsetFetchResponse(t *testing.T) {
 
 func TestNormalOffsetFetchResponse(t *testing.T) {
 	response := OffsetFetchResponse{}
-	response.AddBlock("t", 0, &OffsetFetchResponseBlock{0, "md", ErrRequestTimedOut})
+	response.AddBlock("t", 0, &OffsetFetchResponseBlock{0, 0, "md", ErrRequestTimedOut})
 	response.Blocks["m"] = nil
 	// The response encoded form cannot be checked for it varies due to
 	// unpredictable map traversal order.
 	testResponse(t, "normal", &response, nil)
 
 	responseV2 := OffsetFetchResponse{Version: 2, Err: ErrInvalidRequest}
-	responseV2.AddBlock("t", 0, &OffsetFetchResponseBlock{0, "md", ErrRequestTimedOut})
+	responseV2.AddBlock("t", 0, &OffsetFetchResponseBlock{0, 0, "md", ErrRequestTimedOut})
 	responseV2.Blocks["m"] = nil
 	// The response encoded form cannot be checked for it varies due to
 	// unpredictable map traversal order.
